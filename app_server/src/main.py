c6030e16c01784387ff25fb31055a723ed8a13f8
Andrzej Michnowski <am394469@students.mimuw.edu.pl>
2022-03-14 17:33:44 +0100

    c

--- app_server/src/main.py
+++ app_server/src/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, response
+import os
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/health")
+async def root(response: Response):
+    status_code = os.getenv("RETURN_CODE")
+    if status_code:
+        status_code = int(status_code)
+    else:
+        status_code = 200
+    response.status_code = status_code
+    return {"healthcheck_code": status_code}
